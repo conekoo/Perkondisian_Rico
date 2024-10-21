97bea9a024e73cb50047b3bb02e8154498071bd3
conekoo <162538330+conekoo@users.noreply.github.com>
2024-10-21 20:04:22 +0700

    Add files via upload

---
+++ Tugas Perkondisian_Rico Satria_5 MIB.py
@@ -0,0 +1,159 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6ea28047-c082-4bf6-bcfa-b863aa389fbb",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "a = 33\n",
+    "b = 100\n",
+    "if (a>b):\n",
+    "    print(\"Nilai A lebih Besar dari B\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "018814bf-1dbf-4fe9-8949-0a11302fe554",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "a = 33\n",
+    "b = 100\n",
+    "if (a>b):\n",
+    "    print(\"Nilai A lebih Besar dari B\")\n",
+    "else:\n",
+    "    print(\"Nilai A lebih Kecil dari B\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ca2f73a9-d20b-4a24-bb99-71b61ef819cb",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "a = 100\n",
+    "b = 50\n",
+    "if (a>b):\n",
+    "    print(\"Nilai A lebih Besar dari B\")\n",
+    "else:\n",
+    "    print(\"Nilai A lebih Kecil dari B\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5ec19e0e-5eaa-4f3d-ad94-b6443c2ac231",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "a = 100\n",
+    "b = 50\n",
+    "if (a>b):\n",
+    "    print(\"Nilai A lebih Besar dari B\")\n",
+    "elif (a<b):\n",
+    "    print(\"Nilai A lebih Kecil dari B\")\n",
+    "else:\n",
+    "    print(\"Nilai A Sama Dengan B\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1564c08f-1c34-4709-9ab1-713069f60002",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "if (a>b) : print(\"A lebih Besar dari B\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "dd9a5670-8d00-450b-8500-155b4a985776",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "a = 10\n",
+    "b = 5\n",
+    "\n",
+    "print(\"A lebih Besar dari B\") if a>b else print(\"A lebih Kecil dari B\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ff5ec71e-f14c-41ee-b46c-71804267b782",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "print(\"A lebih Besar dari B\") if a>b else print(\"A lebih Kecil dari B\") if a<b else print(\"A sama dengan B\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "aecce10d-6d1c-470b-a6f2-a5a8e437b029",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "bilangan = 25\n",
+    "\n",
+    "if (bilangan %2 ==0):\n",
+    "    print(\"Bilangan Genap\")\n",
+    "else:\n",
+    "    print(\"Bilangan Ganjil\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "55dc525d-6f19-42df-892d-1bdb2e9ce169",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "nama = input (\"Masukkan Nama Anda : \")\n",
+    "print (\"Nama Anda Adalah : \" + nama)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5ae7d0a3-ec9b-4f4d-900e-6af2d6c254aa",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "bilangan = int (input(\"Masukkan Bilangan : \"))\n",
+    "\n",
+    "if (bilangan %2 == 0):\n",
+    "    print(bilangan, \"Adalah Bilangan Genap\")\n",
+    "else:\n",
+    "    print(bilangan, \"Adalah Bilangan Ganjil\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
